7d90a369a6bc40d6e905efac94ba67af46c0af99
Pratham Bansal <bansalpratham.bt21cse@pec.edu.in>
2023-10-31 23:16:43 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -5,19 +5,30 @@ const dummyUsers=[];
 function App() {
   const [showResult,setShowResult]=useState(false);
   const [users,setUsers]=useState(dummyUsers);
+  const [error,setError]=useState();
   
   const SubmitHandler=(userInput)=>{
     setShowResult(true);
     const newUser={name:userInput.target[0].value,age:userInput.target[1].value,id:Math.random().toString()};
-    if(newUser.age>0){
+    if(newUser.age.length===0 || newUser.name.length===0 || +newUser.age<1){
+      setError({
+        title:'Invalid input',
+        message: 'Please enter a valid username and age (>0).'
+      })
+      return;
+    }
+    else{
       setUsers((prev)=>{
         return [...prev,newUser];
       })
     }
   }
+  const errorHandler=()=>{
+    setError(null);
+  }
   return (
     <div>
-      <UserForm submitControl={SubmitHandler} />
+      <UserForm submitControl={SubmitHandler} error={error} errorHandler={errorHandler}/>
       {users.length>0 && showResult && <Result data={users}/>}
     </div>
   );
--- src/components/Users/UserForm.js
+++ src/components/Users/UserForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from './UserForm.module.css';
+import ErrorModal from '../UI/ErrorModal';
 const UserForm=(props)=>{
     const [userName,setUserName]=useState('');
     const [userAge,setUserAge]=useState('');
@@ -18,15 +19,18 @@ const UserForm=(props)=>{
         setUserAge('');
     }
     return(
-        <Card className={classes.input}>
-        <form onSubmit={submitHandler}>
-            <label htmlFor='fullName'>Username</label>
-            <input type='text' name='fullName' id='fullName' value={userName} onChange={userNameHandler}></input>
-            <label htmlFor='age'>Age</label>
-            <input type='number' name='age' id='age' value={userAge} onChange={userAgeHandler}></input>
-            <Button type="submit" >Add User</Button>
-        </form>
-        </Card>
+        <div>
+            {props.error && <ErrorModal title={props.error.title} message={props.error.message} errorHandler={props.errorHandler}/>}
+            <Card className={classes.input}>
+            <form onSubmit={submitHandler}>
+                <label htmlFor='fullName'>Username</label>
+                <input type='text' name='fullName' id='fullName' value={userName} onChange={userNameHandler}></input>
+                <label htmlFor='age'>Age</label>
+                <input type='number' name='age' id='age' value={userAge} onChange={userAgeHandler}></input>
+                <Button type="submit" >Add User</Button>
+            </form>
+            </Card>
+        </div>
     );
 }
 export default UserForm;
